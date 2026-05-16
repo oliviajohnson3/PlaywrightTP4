6d7b5e394ce1657ea0f6e5fdb06c96a33053e195
oliviajohnson3 <olivia.johnson3@gmail.com>
2026-05-16 11:43:29 +0200

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,7 @@ import { defineConfig, devices } from '@playwright/test';
  
 export default defineConfig({
   testDir: './tests',
-  timeout: 30000,
+  timeout: 20000,
   retries: 1,
   workers: 1,
  
